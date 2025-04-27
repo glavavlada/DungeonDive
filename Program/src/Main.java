aa711e0c906a9465ab8d9aed707e51d79d074227
Jacob <Jacobh1@uw.edu>
2025-04-26 17:06:10 -0700

    Added some javadoc

--- Program/src/Main.java
+++ Program/src/Main.java
@@ -1,6 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter
+
+/**
+ * Main class where the program is run.
+ *
+ * @author Jacob Hilliker
+ * @author Emanuel Feria
+ * @author Vladyslav Glavatskyi
+ * @version 4/26/2025
+ */
 public class Main {
+    /**
+     * main method where the program is run.
+     *
+     * @param args command line argument.
+     */
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
--- Program/src/Model.java
+++ Program/src/Model.java
@@ -1,2 +1,10 @@
+/**
+ *
+ *
+ * @author Jacob Hilliker
+ * @author Emanuel Feria
+ * @author Vladyslav Glavatskyi
+ * @version 4/26/2025
+ */
 public class Model {
 }
